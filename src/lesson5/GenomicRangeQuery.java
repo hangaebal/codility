f71f616dbefb84c5e8f59301523f3b3ec505632e
hangaebal <hangaebal@gmail.com>
2016-06-07 18:14:28 +0900

    Lesson 5 Prefix Sums
    - GenomicRangeQuery

--- src/lesson5/GenomicRangeQuery.java
+++ src/lesson5/GenomicRangeQuery.java
@@ -1,5 +1,7 @@
 package lesson5;
 
+import java.util.Arrays;
+
 /**
 A DNA sequence can be represented as a string consisting of the letters A, C, G and T,
 which correspond to the types of successive nucleotides in the sequence.
@@ -74,7 +76,7 @@ DNA 서열은 N개의 문자로 구성 된 비어있지 않은 문자열 S = S[0
 K번째 쿼리(0 ≤ K < M)는
 P[K]와 Q[K] 사이의 DNA 서열이 포함된 뉴클레오티드의 가장 작은 'impact factor'를 찾기를 요구한다.
 
-예를 들어 문자열 S = CAGCTA 이고 array P, Q 가 다음과 같다면:
+예를 들어 문자열 S = CAGCCTA 이고 array P, Q 가 다음과 같다면:
 	P[0] = 2    Q[0] = 4
 	P[1] = 5    Q[1] = 5
 	P[2] = 0    Q[2] = 6
@@ -98,7 +100,7 @@ N개의 문자로 구성된 비어있지 않은 문자열 S와 M개의 정수로
 - 레코드 (Pascal),
 - 정수 배열 (그 밖의 다른 프로그래밍 언어).
 
-예를 들어 문자열 S = CAGCTA 이고 array P, Q 가 다음과 같다면:
+예를 들어 문자열 S = CAGCCTA 이고 array P, Q 가 다음과 같다면:
 	P[0] = 2    Q[0] = 4
 	P[1] = 5    Q[1] = 5
 	P[2] = 0    Q[2] = 6
@@ -118,5 +120,59 @@ N개의 문자로 구성된 비어있지 않은 문자열 S와 M개의 정수로
 배열의 요소는 수정할 수 있다.
  */
 public class GenomicRangeQuery {
+	public static String S = "CAGCCTA";
+	public static int[] P = {2,5,0};
+	public static int[] Q = {4,5,6};
+
+	public static int[] solution(String S, int[] P, int[] Q) {
+		int N = S.length();
+		int M = P.length;
+
+		int[] result = new int[M];
+		// 2차원 배열에 index 증가에 따른 char 별 누적 카운트
+		// [0] = A, [1] = C, [2] = G, [3] = T
+		int[][] accumulation = new int[4][N + 1];
+
+		for (int i = 0; i < N; i ++) {
+			// 문자를 찾으면 누적 카운트 ++
+			switch (S.charAt(i)) {
+				case 'A': accumulation[0][i + 1]++; break;
+				case 'C': accumulation[1][i + 1]++; break;
+				case 'G': accumulation[2][i + 1]++; break;
+				case 'T': accumulation[3][i + 1]++; break;
+			}
+
+			// 마지막 반복 제외
+			if (i < N - 1) {
+				// 현재 index 까지의 누적 카운트를 다음 index에도 누적
+				for (int j = 0; j < 4; j++) {
+					accumulation[j][i + 2] = accumulation[j][i + 1];
+				}
+			}
+		}
+
+
+		for (int i = 0; i < M; i ++) {
+			int start = P[i];
+			int end = Q[i];
+
+			for (int j = 0; j < 4; j++) {
+				// 끝 지점과 시작 지점의 누적 카운트가 다르다면 (= 해당 문자가 있다면)
+				if (accumulation[j][start] != accumulation[j][end + 1]) {
+					// 발견시 최소값에 삽입
+					result[i] = j + 1;
+					// 최소 factor 부터 확인하므로, 발견시 이후 factor는 확인할 필요 없음
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] result = solution(S, P, Q);
+		System.out.println(Arrays.toString(result));
+	}
 
 }
